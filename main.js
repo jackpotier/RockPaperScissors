1d3647f0dbe0beb8a55cebade54b235e36b8909f
Jack Potier <jackpotierschool@gmail.com>
2022-11-18 15:15:14 +1300

    displays player and computer selections + rounds

--- main.js
+++ main.js
@@ -1,4 +1,30 @@
 function getComputerChoice(){
     options = ['rock','paper','scissors'];
-    window.ComputerSelection = options[Math.floor(Math.random()*options.length)];
+    window.computerSelection = options[Math.floor(Math.random()*options.length)];
 }
+
+function playRound(){
+    roundNumber = 1;
+    // availableChoices is a node list. It looks and acts much like an array.
+   const availableChoices = document.querySelectorAll('button');
+    // we use the .forEach method to iterate through each button
+    availableChoices.forEach((button) => {
+        // and for each one we add a 'click' listener
+        button.addEventListener('click', () => {
+            //If button is clicked, the ID becomes the player's selection for the round
+            let playerSelection = button.id;
+            //The computer's choice is selected
+            getComputerChoice();
+            //Console log round information for developers sake
+            console.log("Round",roundNumber)
+            console.log("Player chooses:",playerSelection);
+            console.log("Computer chooses:",computerSelection);
+            //Next round is generated
+            roundNumber+=1;
+        });
+    });
+
+    //
+ };
+
+ playRound()
